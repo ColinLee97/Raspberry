520ecf05338a4a8ae6371a78cd67d715f7634686
ColinLee97 <colin97.lee@gmail.com>
2016-06-13 20:41:18 +0800

    修改排版 增强css兼容性 JS添加注释
    修改了首页封面图的breakpoint设定，现在是xs、sm下都显示垂直图，md、lg下显示大图，与翻页按钮相对应。
    introduction部分的css，给transition加上了-webkit-等兼容性前缀（之前漏了）
    general.css删除空部分。
    navtrans.js添加详细中文注释
    inPageScroll.js添加详细中文注释

--- 期末大作业/js/navtrans.js
+++ 期末大作业/js/navtrans.js
@@ -18,18 +18,26 @@ function debounce(func, wait, immediate) {
 	};
 };
 
+//定义全局变量target
 var target;
 
+//当网页加载或者拖拉窗口尺寸时，将封面海报的四分之三高度赋值给target
 $(window).on("load resize", function() {
 	target = $(".header").height()*3/4;
 });
 
+//定义变更类的函数，用于绑定侦听器
+//加入debounce来限制最多每17毫秒执行一次函数，避免过分占用CPU
 var checkScroll = debounce(function() {
+	//判断页面位置是否大于窗口高度
 	if ($(window).scrollTop() > target) {
+		//当结果为真，即滚到封面海报以下时，移除.navbar-transparent类
 		$(".navbar").removeClass("navbar-transparent");
 	} else {
+		//反之，则添加透明类
 		$(".navbar").addClass("navbar-transparent");
 	};
 },17);
 
+//将函数与窗口滚动绑定在一起
 $(window).on("scroll", checkScroll);
