f9a8d20273bec31b624bc39aa0d9efcc7e090f39
ColinLee97 <colin97.lee@gmail.com>
2016-06-14 22:05:14 +0800

    小修小改

--- 期末大作业/js/inPageScroll.js
+++ 期末大作业/js/inPageScroll.js
@@ -1,19 +1,24 @@
 //所有含有.inPage类名的超链接<a>，给点击事件添加新动作
 $("a[class~='inPage']").on("click",function(e){
+	
 	//阻止默认动作，即跳转到超链接所指向的页面
 	e.preventDefault();
+	
 	//获取超链接的href属性，即要跳转到的页面位置
 	var dest = $(this).attr("href");
+	
 	//需要对高度做出的补偿，由HTML部分定义
 	//这是为了通用性，因为要在不同排版的页面中
 	//使用这个代码，所以有这么一个属性
 	var minus = $(this).attr("minus");
+	
 	//判断页面目标位置，若为#（回到顶部）则为零
 	if (dest=="#") {
 		var target = 0;
 	} else {
 		var target = $(dest).offset().top-minus;
 	}
+	
 	//通过jQuery来不断更改当前滚动位置，以实现动画效果
 	$("body").animate({scrollTop: target}, "slow");
 });
--- 期末大作业/js/navtrans.js
+++ 期末大作业/js/navtrans.js
@@ -4,18 +4,18 @@
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
 function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
 
 //定义全局变量target
@@ -23,20 +23,23 @@ var target;
 
 //当网页加载或者拖拉窗口尺寸时，将封面海报的四分之三高度赋值给target
 $(window).on("load resize", function() {
-	target = $(".header").height()*3/4;
+    target = $(".header").height()*3/4;
 });
 
 //定义变更类的函数，用于绑定侦听器
 //加入debounce来限制最多每17毫秒执行一次函数，避免过分占用CPU
 var checkScroll = debounce(function() {
-	//判断页面位置是否大于窗口高度
-	if ($(window).scrollTop() > target) {
-		//当结果为真，即滚到封面海报以下时，移除.navbar-transparent类
-		$(".navbar").removeClass("navbar-transparent");
-	} else {
-		//反之，则添加透明类
-		$(".navbar").addClass("navbar-transparent");
-	};
+
+    //判断页面位置是否大于窗口高度
+    if ($(window).scrollTop() > target) {
+
+        //当结果为真，即滚到封面海报以下时，移除.navbar-transparent类
+        $(".navbar").removeClass("navbar-transparent");
+    } else {
+
+        //反之，则添加透明类
+        $(".navbar").addClass("navbar-transparent");
+    };
 },17);
 
 //将函数与窗口滚动绑定在一起
